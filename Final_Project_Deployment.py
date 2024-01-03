6a9ad4e8dd1aeac4472dae101563885ffa1b29ea
BAYU AFRIZAL <118601355+bayuafrizal@users.noreply.github.com>
2024-01-03 23:19:15 +0700

    Final_Project_Deployment.py

--- Final_Project_Deployment.py
+++ Final_Project_Deployment.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 #Import data
-pickle_in = open(pickle_in = open('bayuafrizal/final_project_matchprimerleague/model.pkl', 'rb')
+pickle_in = open('bayuafrizal/final_project_matchprimerleague/model.pkl', 'rb')
 classifier = pickle.load(pickle_in) 
 
 #Data Preprocessing
